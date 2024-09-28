0a79c5e1a0d56ce43f288e92da33784b877fa344
jacob-corletto <jacob.corletto@csu.fullerton.edu>
2024-09-27 18:48:00 -0700

    testing

---
+++ src/app/home/page.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <h1>Welcome to the Setlist checklist</h1>
+      </div>
+      <div>
+        <h2>Upcoming Shows</h2>
+      </div>
+    </div>
+  );
+}
