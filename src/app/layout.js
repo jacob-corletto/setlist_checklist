f54b3614a660f5dad6f182d903ea763b21aafb9d
marsxnow <brat@raxs-MacBook-Pro.local>
2024-09-27 18:31:07 -0700

    Added tailwind css to the project

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,31 +1,31 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
-import styles from '../Styles/Nav.module.css'
+import { Inter } from "next/font/google";
+import "./globals.css";
+// import styles from '../Styles/Nav.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Setlist Checklist',
-  description: 'The checklist for your next concert',
-}
+  title: "Setlist Checklist",
+  description: "The checklist for your next concert",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <nav className={styles.nav}>
-          <h1 className={styles.h1}>logo</h1>
-          <ul className={styles.ul}>
-            <li className={styles.li}>
-              <a className={styles.a} href="/about">About</a>
+      <body>
+        <nav className="bg-black border-gray-200 dark:bg-gray-900">
+          <h1>logo</h1>
+          <ul>
+            <li>
+              <a href="/about">About</a>
             </li>
-            <li className={styles.li}>
-              <a className={styles.a} href='/'>home</a>
+            <li>
+              <a href="/">home</a>
             </li>
           </ul>
         </nav>
         {children}
       </body>
     </html>
-  )
+  );
 }
