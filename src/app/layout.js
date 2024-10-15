22d913b778c7f234031ac27c9fafb5f9193b8dfd
raxelportiz <raxelportiz@gmail.com>
2024-10-15 08:42:18 +0000

    bug with tailwind class

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,31 +26,28 @@ export default function RootLayout({ children }) {
         </nav>
         {children}
         
-<div class="flex flex-col w-full items-center justify-between">
-<footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800 absolute bottom-0 left-50">
-    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Setlist Checklist</a>. All Rights Reserved.
+<div className="flex flex-col w-full items-center justify-between">
+<footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800 absolute bottom-0 left-50">
+    <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Setlist Checklist</a>. All Rights Reserved.
     </span>
-    <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+    <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
-            <a href="#" class="hover:underline me-4 md:me-6">About</a>
+            <a href="#" className="hover:underline me-4 md:me-6">About</a>
         </li>
         <li>
-            <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+            <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
         </li>
         <li>
-            <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
+            <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
         </li>
         <li>
-            <a href="#" class="hover:underline">Contact</a>
+            <a href="#" className="hover:underline">Contact</a>
         </li>
     </ul>
     </div>
 </footer>
 </div>
-
-
-
       </body>
     </html>
   );
