7e93356d299c4ec5d5f0ad4e17497b607f5b5e7f
raxelportiz <raxelportiz@gmail.com>
2024-10-19 23:51:16 +0000

    Created footer component was swapped out in layout.js - main changes need to be done in whereever {children} is at

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import DropdownMenu from "@/components/DropdownMenu";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,42 +29,7 @@ export default function RootLayout({ children }) {
           </ul>
         </nav>
         {children}
-
-        <div className="flex flex-col w-full items-center justify-between">
-          <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800 bottom-0 left-50">
-            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-              <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-                © 2023{" "}
-                <a href="https://flowbite.com/" className="hover:underline">
-                  Setlist Checklist
-                </a>
-                . All Rights Reserved.
-              </span>
-              <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-                <li>
-                  <a href="#" className="hover:underline me-4 md:me-6">
-                    About
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="hover:underline me-4 md:me-6">
-                    Privacy Policy
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="hover:underline me-4 md:me-6">
-                    Licensing
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="hover:underline">
-                    Contact
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </footer>
-        </div>
+        <Footer/>
       </body>
     </html>
   );
