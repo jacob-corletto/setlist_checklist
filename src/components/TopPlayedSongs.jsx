e8c57f33f86dd158545ae8558642bb3c30ea3651
jacob-corletto <jacob.corletto@csu.fullerton.edu>
2024-11-21 12:07:06 -0800

    added useState and useEffect

--- src/components/TopPlayedSongs.jsx
+++ src/components/TopPlayedSongs.jsx
@@ -2,20 +2,19 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 const TopPlayedSongs = ({ setlists }) => {
-  // Create a Map to store song counts
-  const songCounts = new Map();
+  const [songCounts, setSongCounts] = useState(new Map());
 
-  // Iterate over each setlist and its songs
-  setlists.forEach((setlist) => {
-    setlist.songs.forEach((song) => {
-      songCounts.set(song, (songCounts.get(song) || 0) + 1);
+  useEffect(() => {
+    const newSongCounts = new Map();
+    setlists.forEach((setlist) => {
+      setlist.songs.forEach((song) => {
+        newSongCounts.set(song, (newSongCounts.get(song) || 0) + 1);
+      });
     });
-  });
+    setSongCounts(newSongCounts);
+  }, [setlists]);
 
-  // Sort the Map by value in descending order
   const sortedSongs = [...songCounts.entries()].sort((a, b) => b[1] - a[1]);
-
-  // Get the top 20 most popular songs
   const top20 = sortedSongs.slice(0, 20);
 
   return (
@@ -26,7 +25,7 @@ const TopPlayedSongs = ({ setlists }) => {
           <ul className="grid grid-cols-2 gap-1">
             {top20.map(([song, count], index) => (
               <li key={index} className="py-2">
-                {song} ({count})
+                {song}
               </li>
             ))}
           </ul>
