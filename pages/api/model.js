eef6f66d3f8a69988831a725f3b690bf99c7aa80
marsxnow <raxelportiz@gmail.com>
2024-12-06 12:05:23 -0800

    publishing our model

---
+++ pages/api/model.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import * as tf from "@tensorflow/tfjs";
+import { load } from "protobufjs";
+
+export default function Model() {
+  const [model, setModel] = useState(null);
+  const [summary, setSummary] = useState("");
+
+  useEffect(() => {
+    const loadModel = async () => {
+      const model = await tf.loadLayersModel("model/model.json");
+      setModel(model);
+      const summary = [];
+      model.summary(null, (line) => summary.push(line));
+      setSummary(summary.join("\n"));
+    };
+    loadModel();
+  }, []);
+  return (
+    <div>
+      <h1>Model</h1>
+      <pre>{summary}</pre>
+    </div>
+  );
+}
